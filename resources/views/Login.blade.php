96a7fad7c40bf07bd0675abd85389635b3c99c81
Jairo2103 <jairodanielramosf@gmail.com>
2024-11-01 22:50:24 -0500

    goku

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -1,49 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pagina social</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
-    <div class="container">
-        <div class="titulo">
-            <h1> LOGIN </h1>
-            <div class="subtitulo">
-                <p> Bienvenido, para ingresar llenar tus nuevos datos </p>
-            </div>
-            <div class="nombres">
-                
-                <label for="name">Nombres: 
-                    <input type="text" name="Nombres">
-                </label>
-            </div>
-            <div class="apellidos">
-                <label for="firstname">Apellidos:
-                    <input type="text" name="Apellidos">
-                </label>
-            </div>
-            <div class="Correo">
-                <label for="firstname">Correo:
-                    <input type="email" name="Correo">
-                </label>
-            </div>
-            <div class="password">
-                <label for="password">Password:
-                    <input type="password" name="password">
-                </label>
-            </div>
-            <div class="fecha_nacimiento">
-                <label for="day">Dia:
-                    <select name="" id="">
-                        <option value="1"></option>
-                    </select>
-                </label>
-            </div>
-        </div>
+  <div class="login-container">
+    <h1>LOGIN</h1>
+    <p>How do I get started lorem ipsum dolor at?</p>
+    
+    <div class="input-group">
+      <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
+      <input type="text" placeholder="Username">
     </div>
 
+    <div class="input-group">
+      <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24"><path d="M12 17c1.66 0 3-1.34 3-3v-2c0-1.66-1.34-3-3-3s-3 1.34-3 3v2c0 1.66 1.34 3 3 3zm6-3v2c0 3.31-2.69 6-6 6s-6-2.69-6-6v-2c0-3.31 2.69-6 6-6s6 2.69 6 6z"/></svg>
+      <input type="password" placeholder="Password">
+    </div>
+
+    <button class="login-btn">Login Now</button>
+
+    <p class="social-login">Login with Others</p>
+    
+    <a href="#" class="social-btn google">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google Logo">
+      Login with Google
+    </a>
+    <a href="#" class="social-btn facebook">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook Logo">
+      Login with Facebook
+    </a>
+  </div>
 </body>
 </html>
